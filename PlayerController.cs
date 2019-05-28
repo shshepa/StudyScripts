f47cf57d3b67fce345f94496c08524b471d91b0b
unknown <style6ykawka@yandex.ru>
2019-05-28 20:23:14 +0300

    Signed-off-by: unknown <style6ykawka@yandex.ru>

---
+++ PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof (PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{     
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSpeed = 3f;   
+
+    private PlayerMotor motor;
+
+    void Start () {
+        motor = GetComponent <PlayerMotor> ();
+    }
+    void Update()
+    {
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 movHor = transform.right * xMov;
+        Vector3 movVer = transform.forward * zMov;
+        Vector3 velocity = (movHor + movVer).normalized * speed;
+
+        motor.Move (velocity);
+
+        float yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 rotation = new Vector3(0f, yRot, 0f) * lookSpeed;
+        motor.Rotate(rotation);
+
+    }
+    
+}
---
+++ PlayerMotor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    void Start()
+    {
+        rb = GetComponent <Rigidbody> ();
+
+    }
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+        void FixedUpdate()
+    {
+        PerformMove();
+        PerformRotation();
+
+    }
+
+    void PerformMove()
+    {
+        if (velocity != Vector3.zero)
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler (rotation));
+
+    }
+}
